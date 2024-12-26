43dc0c4b52924125d6bbc3ed36cbb941eb26f279
Ali <aliahmad5496543@gmail.com>
2024-12-26 21:33:59 +0500

    Initial commit

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    // public function index():Response
+    public function index(): View
+    {
+        // return response('Hello, World!');
+        return view('chirps', [
+            // 
+        ]);
+    }
+
+    
+}
